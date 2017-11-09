709a016965f907a167bad61ed9976b53c016fb62
Paul Girard <paul.girard@sciencespo.fr>
2017-11-09 16:31:02 +0100

    corrected some filed names and removing ensemble filtering

--- modules/groupEnsemblesAndExport_mod.py
+++ modules/groupEnsemblesAndExport_mod.py
@@ -19,11 +19,11 @@ csv_line = lambda dat,keys: ",".join([format_csv(format_field(dat[k])) for k in 
 
 if __name__ == "__main__":
 
-    fields = ["_id", "acquisition_year", "acquisition_mode", "date_creation", "type", "domain", "domain_leaf", "domain_deno_for_grid", "domain_description_mst", "collection", "collection_department", "recap_authors", "authors_list", "authors_nationality", "authors_birth_death", "authors_name_complement", "title_notice", "title_list", "comments", "recap_description", "all_realized_operations_history", "hanging_history", "hanging_history_m34", "temporary_exhibitions_history_m30", "exposition_out_of_folder", "expositions", "expositions_without_current", "inscriptions", "authors"]
+    fields = ["_id", "acquisition_year", "acquisition_mode", "date_creation", "type", "ensemble_id","domain", "domain_leaf", "domain_deno_for_grid", "domain_description_mst", "collection", "collection_department", "recap_authors", "authors_list", "authors_nationality", "authors_birth_death", "authors_name_complement", "title_notice", "title_list", "comments", "recap_description", "all_realized_operations_history", "hanging_history", "hanging_history_m34", "temporary_exhibitions_m30", "exposition_out_of_folder", "expositions", "expositions_without_current", "inscriptions", "authors"]
     med_fields = fields + ["url", "copyright", "legend", "max_width", "max_height"]
-    array_fields = ["all_realized_operations_history", "hanging_history", "hanging_history_m34", "temporary_exhibitions_history_m30", "expositions", "authors"]
+    array_fields = ["all_realized_operations_history", "hanging_history", "hanging_history_m34", "temporary_exhibitions_m30", "expositions", "authors"]
 
-    artworks = open("uniq_artworks.csv", "w")
+    artworks = open("artworks.csv", "w")
     print >> artworks, ",".join(fields)
     images = open("images_with_urls.csv", "w")
     print >> images, ",".join(med_fields)
@@ -31,34 +31,38 @@ if __name__ == "__main__":
     ensembles = {}
     inventory = {}
     uniq_doubles = {}
-    re_inv = re.compile(r"((?: (?:FH|AP|DOC))?|AM|DOC|GOB|JP|GMTT|LUX[.O ]*|MV|INV\.?|RF|N|LP|Fh|MI)[\s]*([A-Z]*[\d\-\.]+)")
+    #re_inv = re.compile(r"((?: (?:FH|AP|DOC))?|AM|DOC|GOB|JP|GMTT|LUX[.O ]*|MV|INV\.?|RF|N|LP|Fh|MI)[\s]*([A-Z]*[\d\-\.]+)")
     for a in db["Artwork"].find():
         dat = {}
         for key in fields:
             dat[key] = a.get(key, "")
         dat["description"] = dat["domain_description_mst"].replace(dat["domain"], "").replace(u"|", ", ")
 
+        # skip = False
+        # if a.get("ensemble_id", ""):
+        #     if a["ensemble_id"] in ensembles:
+        #         skip = True
+        #     else:
+        #         ensembles[a["ensemble_id"]] = True
+        inventory_id = a.get("inventory", a.get("deposit_number", ""))
         skip = False
-        if a.get("ensemble_id", ""):
-            if a["ensemble_id"] in ensembles:
+        if inventory_id != '':
+            if inventory_id in inventory:    
+                if inventory_id not in uniq_doubles:
+                    print "skip duo inventory", inventory_id
+                    uniq_doubles[inventory_id] = True
                 skip = True
             else:
-                ensembles[a["ensemble_id"]] = True
-        if re_inv.match(a.get("inventory", "")):
-            inv_id = "#".join(re_inv.search(a["inventory"]).groups() + (str(a.get("acquisition_year", "")),))
-            if inv_id in inventory:
-                if not skip:
-                    if inv_id not in uniq_doubles:
-                        print "skip duo inventory", inv_id
-                        uniq_doubles[inv_id] = True
-                skip = True
-            else:
-                inventory[inv_id] = True
-            dat["inventory_id"] = inv_id
+                inventory[inventory_id] = True
+
+            dat["inventory_id"] = inventory_id
         else:
             print "missing inv", a.get("inventory")
         for field in array_fields:
-            dat[field] = '#'.join(dat[field])
+            if field=="all_realized_operations_history":
+                dat[field] = ';sep;'.join(dat[field])    
+            else:
+                dat[field] = '#'.join(dat[field])
         if not skip:
             print >> artworks, csv_line(dat, fields)
         for mid in a["medias"]:
