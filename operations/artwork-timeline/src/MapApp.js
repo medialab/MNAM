2a54edba20ddb95c2ecba99514d50e1e07ff12a1
Ian Ardouin-Fumat <ian.ardouin@gmail.com>
2017-11-09 23:12:04 +0100

    performance

--- operations/artwork-timeline/src/MapApp.js
+++ operations/artwork-timeline/src/MapApp.js
@@ -1,9 +1,10 @@
 /*
 
-  forces
+  remove unborn nodes
+  remove unknown from locations
   playback
   location queues
-  sous-locations
+  sublocations
   color coding
 
 */
@@ -47,7 +48,7 @@ class MapApp extends Component {
     this.state = {
       artworks: [],
       timeRange: [10000000000000, 0],
-      artworkCount: 1000,
+      artworkCount: 10000,
       stopList: [
         150000000030351,
         150000000029858,
@@ -68,9 +69,12 @@ class MapApp extends Component {
         150000000012643
       ],
       nodes: [],
+      nodeGrid: [],
       locations: [],
       currentDate: new Date(0),
-      speed: 1000*60*60*24
+      speed: 1000*60*60*24,
+      width: 100,
+      height: 100
     }
 
     this.init = this.init.bind(this)
@@ -94,6 +98,11 @@ class MapApp extends Component {
 
     const artworks = props.data
       .filter((d, i) => i < artworkCount)
+      .filter((d, i) => {
+        return d.opt_field.filter(o => {
+          return !!o.opt_branch && o.opt_branch !== 'unknown'
+        }).length > 5
+      })
       .map(a => {
         const operations = a.opt_field
           .filter(o => {
@@ -114,6 +123,8 @@ class MapApp extends Component {
           }
       })
 
+    console.log(artworks.length)
+
     timeRange[0] = artworks.reduce((a, b) => {
       const minOperations = b.operations.reduce((c, d) => {
         return Math.min(c, new Date(d.date).getTime())
@@ -124,8 +135,6 @@ class MapApp extends Component {
     timeRange[1] = Date.now()
 
 
-    // THREE.js
-
     var scene = new THREE.Scene()
     var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 )
     camera.position.set(0, 0, (height/2.0) / Math.tan(Math.PI*35.0 / 180.0))
@@ -134,15 +143,6 @@ class MapApp extends Component {
     var renderer = new THREE.WebGLRenderer()
     renderer.setSize( window.innerWidth, window.innerHeight )
     this.refs.wrapper.appendChild( renderer.domElement )
-
-    // var geometry = new THREE.CircleGeometry( 100, 32 )
-    // var material = new THREE.MeshBasicMaterial( { color: 0xffff00, side:THREE.DoubleSide } )
-    // var circle = new THREE.Mesh( geometry, material )
-    // scene.add( circle )
-
-    // const nodes = artworks.map((a, i) => {
-    //   return new Node(a, scene, width, height)
-    // })
 
     var particleSystem, uniforms, geometry
     var particles = 100000
@@ -194,11 +194,10 @@ class MapApp extends Component {
         locationMap[o.opt_branch] ++
       })
     })
-    console.log('aga', locationMap)
+    console.log('locationMap', locationMap)
 
     let theta = 0
     let rad = 300
-    console.log('aga')
     const locations = Object.keys(locationMap).map(l => {
       const location = locationMap[l]
       const geometry = new THREE.CircleGeometry(50, 32)
@@ -206,7 +205,8 @@ class MapApp extends Component {
       const circle = new THREE.Mesh(geometry, material)
       circle.position.set(Math.cos(theta) * rad, Math.sin(theta) * rad, 0)
       circle.locationId = l
-      scene.add(circle)
+      circle.count = 0
+      // scene.add(circle)
       theta += Math.PI * 2 / Object.keys(locationMap).length
       return circle
     })
@@ -239,7 +239,10 @@ class MapApp extends Component {
       particleSystem,
       nodes,
       locations,
-      speed
+      speed,
+      width,
+      height,
+      nodeGrid
     } = this.state
 
     let {
@@ -253,19 +256,60 @@ class MapApp extends Component {
 
     const attributes = particleSystem.geometry.attributes
 
+    const gridResolution = 60
+    const newNodeGrid = new Array(gridResolution)
+      .fill(null)
+      .map(row => new Array(gridResolution)
+        .fill(null)
+        .map(col => new Array())
+      )
+
+
+    let neighboringNodesCount = 0
+    let pushedneighboringNodesCount = 0
     nodes.forEach((n, i) => {
-      n.update(currentDate, locations, nodes)
+      
+      let neighboringNodes = []
+      if (nodeGrid.length > 0) {
+        const x1 = Math.floor((n.position.x + width / 2) / width * gridResolution)
+        const y1 = Math.floor((n.position.y + height / 2) / height * gridResolution)
+        for (let x = x1 - 1; x <= x1 + 1; x++) {
+          for (let y = y1 - 1; y <= y1 + 1; y++) {
+            if (x1 >= 0 && x1 < gridResolution && y1 >= 0 && y1 < gridResolution) {
+              neighboringNodes = neighboringNodes.concat(nodeGrid[x1][y1])
+            }
+          }
+        }
+      }
+
+      // const ratio = Math.floor(neighboringNodes.length / 20)
+      const pos = n.update(currentDate, locations, neighboringNodes)
+      const x = Math.floor((pos.x + width / 2) / width * gridResolution)
+      const y = Math.floor((pos.y + height / 2) / height * gridResolution)
+      if (x >= 0 && x < gridResolution && y >= 0 && y < gridResolution && n.active) {
+        newNodeGrid[x][y].push(n)
+        pushedneighboringNodesCount ++
+      }
+
       attributes.position.array[i * 3 + 0] = n.position.x
       attributes.position.array[i * 3 + 1] = n.position.y
       attributes.position.array[i * 3 + 2] = n.position.z
+      attributes.color.array[i * 4 + 0] = n.color.r
+      attributes.color.array[i * 4 + 1] = n.color.g
+      attributes.color.array[i * 4 + 2] = n.color.b
     })
     attributes.position.needsUpdate = true
+    attributes.color.needsUpdate = true
 
     renderer.render(scene, camera)
-    
+
+    // if (Math.random() < 0.05) console.log(newNodeGrid)
+    // console.log(neighboringNodesCount, pushedneighboringNodesCount)
+
     this.setState({
       ...this.state,
-      currentDate: currentDate + speed
+      currentDate: currentDate + speed,
+      nodeGrid: newNodeGrid
     })
     
     requestAnimationFrame(this.tick)
--- operations/artwork-timeline/src/Node.js
+++ operations/artwork-timeline/src/Node.js
@@ -14,6 +14,7 @@ class Node {
     this.update = this.update.bind(this)
 
     this.currentOperation = null
+    this.currentLocation = null
 
     var geometry = new THREE.CircleGeometry( 8, 32 )
     var material = new THREE.MeshBasicMaterial( { color: Math.random()* 0xffffff } )
@@ -22,18 +23,20 @@ class Node {
     const r = Math.random() * height / 2
 
     this.position = new THREE.Vector3(Math.cos(theta) * r, Math.sin(theta) * r, 0)
-    this.color = new THREE.Color()
-    this.color.setHSL( Math.random(), 1.0, 0.5 )
+    this.color = new THREE.Color(0x000000)
     this.size = 10
 
     this.targetPosition = this.position.clone()
+    this.targetColor = this.color.clone()
 
     this.index = i
 
     this.acc = new THREE.Vector3()
     this.vel = new THREE.Vector3()
     this.damping = 0.85
-    this.attractionToTarget = 0.005
+    this.attractionToTarget = 0.01
+    this.nodeRepulsion = 0.001
+    this.active = false
 
   }
 
@@ -49,40 +52,70 @@ class Node {
       }
     })
     if (latestOperation !== this.currentOperation) {
-      this.currentOperation = latestOperation
+      if (!this.currentOperation) {
+        this.targetColor.setHSL( Math.random(), 1.0, 0.5 )
+        this.active = true
+      }
       const nextLocation = locations.find(l => {
-        return l.locationId === this.currentOperation.opt_branch
+        return l.locationId === latestOperation.opt_branch
       })
       if (!!nextLocation) {
+
+        this.currentLocation = nextLocation
+
+        if (!!this.currentOperation) this.currentOperation.count --
+        nextLocation.count ++
+        
         this.targetPosition.copy(nextLocation.position)
+        if (!this.currentOperation) {
+          const theta = Math.random() * Math.PI * 2
+          const r = Math.sqrt(this.size / 2 * this.currentLocation.count) * (1 + Math.random() * 0.5)
+          const offset = new THREE.Vector3(Math.cos(theta) * r, Math.sin(theta) * r)
+          this.position.copy(nextLocation.position.clone().add(offset))
+        }
+      } else {
+        // console.log('aga')
+        // if (!this.currentOperation) {
+        //   console.log('lol')
+        //   const defaultLocation = locations.find(l => l.locationId === 'CPINTER_storage')
+        //   const theta = Math.random() * Math.PI * 2
+        //   // const r = 50 + Math.random() * 50
+        //   const r = Math.sqrt(this.size * nextLocation.count) * 1.75
+        //   const offset = new THREE.Vector3(Math.cos(theta) * r, Math.sin(theta) * r)
+        //   this.position.copy(defaultLocation.position.clone().add(offset))
+        // }
       }
+      this.currentOperation = latestOperation
     }
 
-    nodes.forEach(n => {
-      if (n !== this) {
-        if (this.position.distanceTo(n.position) < this.size) {
-          const distanceToTarget = this.position.distanceTo(n.position)
-          const force = n.position.clone().sub(this.position)
-          force.negate()
-          force.normalize()
-          force.multiplyScalar((this.size - distanceToTarget) * 0.01)
-          this.acc.add(force)
-        }
+    nodes.forEach((n, i) => {
+      if (this.position.distanceTo(n.position) < this.size) {
+        const distanceToTarget = this.position.distanceTo(n.position)
+        const force = n.position.clone().sub(this.position)
+        force.negate()
+        force.normalize()
+        force.multiplyScalar((this.size - distanceToTarget) * this.nodeRepulsion)
+        this.acc.add(force)
       }
     })
 
+
     const distanceToTarget = this.position.distanceTo(this.targetPosition)
-    const force = this.targetPosition.clone().sub(this.position)
-    force.normalize()
-    force.multiplyScalar(distanceToTarget * this.attractionToTarget)
-    this.acc.add(force)
+    if (!this.currentLocation || distanceToTarget > Math.sqrt(this.size / 3 * this.currentLocation.count)) {
+      const force = this.targetPosition.clone().sub(this.position)
+      force.normalize()
+      force.multiplyScalar(distanceToTarget * this.attractionToTarget)
+      this.acc.add(force)
+    }
 
     this.vel.add(this.acc)
     this.position.add(this.vel)
     this.acc.set(0, 0, 0)
     this.vel.multiplyScalar(this.damping)
 
-    // this.position.lerp(this.targetPosition, 0.15)
+    this.color.lerp(this.targetColor, 0.15)
+
+    return this.position
 
   }
 }
